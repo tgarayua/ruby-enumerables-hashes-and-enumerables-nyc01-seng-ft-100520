4f608572ecc2e3be17dd9c4e40701050963c13b7
Thomas Garayua <tgarayua@gmail.com>
2020-09-25 16:15:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,9 +15,3 @@ passengers.each do |suite, name|
 end
 winner
 end
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
